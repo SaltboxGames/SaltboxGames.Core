de385dfabf2dc014f452f84eae4a1df90d1b6d29
Jonathan Gardner <Jon.Gardner@saltboxgames.com>
2025-07-24 07:47:02 -0700

    Added List<T>.AsSpan extension Added Shuffle Added Crossplatform Deterministic Random

---
+++ Runtime/Extensions/ListExtensions.cs
@@ -0,0 +1,64 @@
+﻿/*
+ * Copyright (c) 2024 SaltboxGames, Jonathan Gardner
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+using System;
+using System.Collections.Generic;
+using SaltboxGames.Core.Utilities;
+
+#if UNITY_2021_1_OR_NEWER
+using UnityEngine.Assertions;
+
+#else
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+#endif
+
+namespace SaltboxGames.Core.Extensions
+{
+    public static class ListExtensions
+    {
+#if UNITY_2021_1_OR_NEWER
+        private static class ListReflectionCache<T>
+        {
+            public static Func<List<T>, T[]> GetItems;
+        }
+        
+        /// <summary>
+        /// Returns a <see cref="Span{T}"/> representing the active portion of the list, without allocations.
+        /// This is equivalent to <c>CollectionsMarshal.AsSpan</c>, but uses cached compiled reflection to access the backing array.
+        /// </summary>
+        /// <typeparam name="T">The element type of the list.</typeparam>
+        /// <param name="list">The list to expose as a span.</param>
+        /// <returns>A span over the elements in the list (up to <c>list.Count</c>).</returns>
+        public static Span<T> AsSpan<T>(this List<T> list)
+        {
+            ListReflectionCache<T>.GetItems ??= Reflection.GetFieldGetter<List<T>, T[]>("_items");
+
+            Assert.IsNotNull(ListReflectionCache<T>.GetItems);
+            
+            T[] items = ListReflectionCache<T>.GetItems(list);
+            return new Span<T>(items, 0, list.Count);
+        }
+#else
+
+        /// <summary>
+        /// Returns a <see cref="Span{T}"/> over the list using the standard <c>CollectionsMarshal.AsSpan</c> API.
+        /// </summary>
+        /// <typeparam name="T">The element type of the list.</typeparam>
+        /// <param name="list">The list to expose as a span.</param>
+        /// <returns>A span over the elements in the list.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Span<T> AsSpan<T>(this List<T> list)
+        {
+            return CollectionsMarshal.AsSpan(list);
+        }
+#endif
+        
+    }
+}
---
+++ Runtime/Utilities/DeterministicRandom.cs
@@ -0,0 +1,94 @@
+﻿/*
+ * Copyright (c) 2024 SaltboxGames, Jonathan Gardner
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+using System;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Threading;
+
+namespace SaltboxGames.Core.Utilities
+{
+    /// <summary>
+    /// Simple Xor32 random~ish number generator.
+    /// </summary>
+    public static class DeterministicRandom
+    {
+        private static int _state = 1;
+        
+        static DeterministicRandom()
+        {
+            // this is dumb, but fast and fun
+            Span<byte> buffer = stackalloc byte[4];
+            RandomNumberGenerator.Fill(buffer);
+
+            int value = Unsafe.As<byte, int>(ref buffer[0]);
+            Seed(value | 1);
+        }
+        
+        public static void Seed(int seed)
+        {
+            seed |= 1;
+            Interlocked.Exchange(ref _state, seed);
+        }
+
+        public static uint NextUInt()
+        {
+            while (true)
+            {
+                int original = _state;
+                int next = original;
+                
+                unchecked
+                {
+                    next ^= next << 13;
+                    next ^= (int)((uint)next >> 17);
+                    next ^= next << 5;
+                }
+
+                if (Interlocked.CompareExchange(ref _state, next, original) == original)
+                {
+                    return unchecked((uint)next);
+                }
+            }
+        }
+
+        public static int NextInt(int max) => (int)(NextUInt() % max);
+        public static int NextInt(int min, int max) => min + NextInt(max - min);
+
+        public static float NextFloat()
+        {
+            uint rnd = NextUInt() & 0x7FFFFFFF;
+            return Unsafe.As<uint, float>(ref rnd) / int.MaxValue;
+        }
+
+        public static float NextFloat(float min, float max)
+        {
+            return min + NextFloat() * (max - min);
+        }
+
+        public static double NextDouble()
+        {
+            ulong hi = NextUInt();
+            ulong lo = NextUInt();
+            ulong combined = (hi << 32) | lo;
+            return combined / (double)ulong.MaxValue;
+        }
+
+        public static double NextDouble(double min, double max)
+        {
+            return min + NextDouble() * (max - min);
+        }
+
+        public static ulong NextULong()
+        {
+            ulong hi = NextUInt();
+            ulong lo = NextUInt();
+            return (hi << 32) | lo;
+        }
+    }
+}
