17002315d68818efa1a77f87b38b6217efbc5dc6
Jon Gardner <jon.gardner@saltboxgames.com>
2026-06-03 22:46:08 -0700

    Added: SPDX-License-Identifier

--- Runtime/Collections/CircularBuffer.cs
+++ Runtime/Collections/CircularBuffer.cs
@@ -1,4 +1,5 @@
-﻿/*
+﻿// SPDX-License-Identifier: MPL-2.0
+/*
  * Copyright (c) 2024-2026 Saltbox Games Cooperative
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
--- Runtime/Utilities/Reflection.Properties.cs
+++ Runtime/Utilities/Reflection.Properties.cs
@@ -1,4 +1,4 @@
-
+// SPDX-License-Identifier: MPL-2.0
 /*
  * Copyright (c) 2024-2026 Saltbox Games Cooperative
  *
