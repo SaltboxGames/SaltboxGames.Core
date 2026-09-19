a0e9d331c42edac297a07d7645ae8846dc1aceb8
Jon Gardner <jon.gardner@saltboxgames.com>
2026-09-19 09:11:54 -0700

    File Array Stack

--- Runtime/Collections/FileStack.cs
+++ Runtime/Collections/FileStack.cs
@@ -1,3 +1,12 @@
+// SPDX-License-Identifier: MPL-2.0
+/*
+ * Copyright (c) 2024-2026 Saltbox Games Cooperative
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -32,7 +41,8 @@ namespace SaltboxGames.Core.Collections
                 fullPath,
                 FileMode.OpenOrCreate,
                 FileAccess.ReadWrite,
-                FileShare.Read);
+                FileShare.Read
+            );
 
             if (stream.Length % elementSize != 0)
             {
